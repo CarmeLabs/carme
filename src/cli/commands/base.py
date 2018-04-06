3a420926cbbb471cb85075341946dc2f7a51109b
Jason Kuruzovich <jkuruzovich@gmail.com>
2018-04-06 13:22:21 -0400

    Changes to base.

--- src/cli/commands/base.py
+++ src/cli/commands/base.py
@@ -1,9 +1,9 @@
 """
 Variables and Functions used by multiple CLI commands
 """
-import os
+import os, subprocess, logging, ruamel.yaml
 
-from os import path, pardir
+from os import path, pardir,getcwd
 
 # Global Constants
 CLI_DIR     =   path.abspath(path.join(path.dirname(__file__), pardir))
@@ -12,6 +12,8 @@ DATA_DIR    =   path.join(BASE_DIR, 'data')
 DOCKER_DIR  =   path.join(DATA_DIR, 'docker')
 LAUNCH_DIR  =   path.join(DATA_DIR, 'launch_files')
 NOTEBOOKS_DIR=  path.join(DATA_DIR, 'notebooks')
+CWD=getcwd()
+
 
 def get_project_root():
     """
@@ -24,3 +26,29 @@ def get_project_root():
             return None
         cd = os.path.dirname(cd)
     return os.path.abspath(cd)
+
+def setup_logger():
+    """
+    Sets up logging
+    """
+    FORMAT = 'carme: [%(levelname)s] %(message)s'
+    logging.basicConfig(level=logging.INFO, format=FORMAT)
+
+
+def bash_command(command, syntax):
+    try:
+        print("Executing "+command+":\n", syntax)
+        result= subprocess.call(syntax, shell=True, executable='/bin/bash')
+        return result
+    except subprocess.CalledProcessError as e:
+        print("error")
+    return(e.output.decode("utf-8"))
+
+def load_yaml(file):
+    try:
+        with open(file, 'r') as yaml:
+            kwargs=ruamel.yaml.round_trip_load(yaml, preserve_quotes=True)
+        return kwargs
+    except subprocess.CalledProcessError as e:
+        print("error")
+    return(e.output.decode("utf-8"))
