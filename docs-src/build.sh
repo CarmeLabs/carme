2c6ffb7dfe731f69795d7bc0109e488e9c9e7273
Jason Kuruzovich <jkuruzovich@gmail.com>
2018-06-26 16:29:53 -0400

    Built documentation

--- docs-src/build.sh
+++ docs-src/build.sh
@@ -1,7 +1,8 @@
+pip install -r requirements.txt
 make html
 rm -rf ../docs
 rm -rf ../doctrees
-mv ../html ../docs 
+mv ../html ../docs
 git add ../docs/*
 git add ../docs-src/*
 git commit -m "Built documentation"
