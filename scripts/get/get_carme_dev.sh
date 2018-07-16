0274a50ac04bce81fb5a5a5680d753e29a1c907d
Jason Kuruzovich <jkuruzovich@gmail.com>
2018-07-16 16:05:43 -0700

    Installing Developer Environment Script

---
+++ scripts/get/get_carme_dev.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/CarmeLabs/carme
+cd carme
+python3 -m venv carme
+source carme/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+pip install -e .
