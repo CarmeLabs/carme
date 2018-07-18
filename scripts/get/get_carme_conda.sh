cd0289f3b52c75bcbfbbd9a7ea5b897f85ed6e47
Jason Kuruzovich <jkuruzovich@gmail.com>
2018-07-18 08:22:17 -0700

    Added Yes to script.

--- scripts/get/get_carme_conda.sh
+++ scripts/get/get_carme_conda.sh
@@ -1,6 +1,6 @@
-conda create --name carme-env
+conda create --name carme-env --yes
 source activate carme-env
-conda install -c anaconda pip
+conda install -c anaconda pip --yes
 wget https://raw.githubusercontent.com/CarmeLabs/carme/master/requirements.txt
 pip uninstall carme --yes
 pip install -r requirements.txt
