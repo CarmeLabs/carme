51090426f06d51161370c45d56a0984910e3598b
Corey Boornazian <coreyb220@gmail.com>
2018-09-22 19:06:03 -0400

    Pylint cleanup.py
    Signed-off-by: Corey Boornazian <coreyb220@gmail.com>

--- src/cli/commands/cleanup.py
+++ src/cli/commands/cleanup.py
@@ -1,10 +1,8 @@
 '''
 Delete and remove all images locally to free up disk space.
 '''
-import os
-import logging
 import click
-from ...modules.base import *
+from ...modules.base import bash_command, setup_logger
 # Set up logger
 setup_logger()
 @click.command()
@@ -12,5 +10,5 @@ def cleanup():
     """
     Delete and remove all images.
     """
-    cmd='docker rmi --force  $(docker images -a -q)'
+    cmd = 'docker rmi --force  $(docker images -a -q)'
     bash_command("Deleting all images", cmd)
