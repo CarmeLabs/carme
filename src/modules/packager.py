989568acc4162b51837d059c6b658611cfdb2c14
Steven vanZyl <rushsteve12@gmail.com>
2018-10-26 14:56:36 -0400

    userexpand -> expanduser

--- src/modules/packager.py
+++ src/modules/packager.py
@@ -16,7 +16,7 @@ from pathlib import Path
 from .yamltools import *
 import re
 # A constant for the downloaded package cache
-PKG_CACHE = os.path.userexpand("~/.carme-cache")
+PKG_CACHE = os.path.expanduser("~/.carme-cache")
 
 # Set up logger
 FORMAT = 'carme: [%(levelname)s] %(message)s'
